4e899e07317759ea28721b5bedb93d758f195dde
Hans Christian Reinl <anything@drublic.de>
2013-01-14 23:05:13 +0100

    Prevent scrolling of main page when overlay is visible

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@
 
 	// Hide overlay when ESC is pressed
 	document.addEventListener('keyup', function (event) {
-		var hash = window.location.hash;
+		var hash = window.location.hash.replace('#', '');
 
 		// If hash is not set
-		if (hash === '' || hash === "!") {
+		if (hash === '' || hash === '!') {
 			return;
 		}
 
@@ -15,6 +15,18 @@
 		if (event.keyCode === 27) {
 			window.location.hash = '!';
 		}
+	}, false);
 
+
+	// When showing overlay, prevent background from scrolling
+	window.addEventListener('hashchange', function () {
+		var hash = window.location.hash.replace('#', '');
+
+		// If hash is set
+		if (hash !== '' && hash !== '!') {
+			document.body.className += ' has-overlay';
+		} else {
+			document.body.className = document.body.className.replace(' has-overlay', '');
+		}
 	}, false);
 }());
