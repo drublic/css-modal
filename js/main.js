d76ae3cf085976340730a5d682a49ea61ce5d22d
Hans Christian Reinl <anything@drublic.de>
2013-01-19 15:29:48 +0100

    Make JS function for preventing body scroll with id more secure
    Check if we are dealing with a modal by checking if the first
    element in selected element has a class `modal-content`.

--- js/main.js
+++ js/main.js
@@ -20,13 +20,17 @@
 
 	// When showing overlay, prevent background from scrolling
 	window.addEventListener('hashchange', function () {
-		var hash = window.location.hash.replace('#', '');
+		var hash = window.location.hash.replace('#', ''),
+		    modalChild;
 
 		// If hash is set
 		if (hash !== '' && hash !== '!') {
 
-			// And has-overlay is not set yet
-			if (!document.body.className.match(/has-overlay/)) {
+			// Get first element in selected element
+			modalChild = document.getElementById(hash).firstElementChild;
+
+			// When we deal with a modal and class `has-overlay` is not set on body yet
+			if (modalChild.className.match(/modal-inner/) && !document.body.className.match(/has-overlay/)) {
 				document.body.className += ' has-overlay';
 			}
 		} else {
