d354475d4dffb0d2ae68432eb2cbf30b91fa9bd3
Hans Christian Reinl <info@drublic.de>
2013-04-19 00:29:25 +0200

    Only set focus if function contains exists

--- modal.js
+++ modal.js
@@ -86,7 +86,9 @@
 
 	// Focus modal
 	modal.setFocus = function (hash) {
-		if (modal.activeElement && !modal.activeElement.contains(hash)) {
+		if (modal.activeElement &&
+				typeof modal.activeElement.contains === 'function' &&
+				!modal.activeElement.contains(hash)) {
 
 			// Set element with last focus
 			modal.lastActive = document.activeElement;
