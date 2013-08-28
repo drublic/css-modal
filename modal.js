ba69eb3938cb542434054acfe996c9da87f0870e
Tobias Barth <4nduril@gmail.com>
2013-08-28 11:57:51 +0200

    Integrated latest changes to match SASS version 1.0.4.

--- modal.js
+++ modal.js
@@ -3,7 +3,7 @@
  * http://drublic.github.com/css-modal
  *
  * @author Hans Christian Reinl - @drublic
- * @version 1.0.1
+ * @version 1.0.4
  */
 
 (function (global) {
@@ -71,6 +71,7 @@
 		var modalElement = document.getElementById(hash);
 		var htmlClasses = document.documentElement.className;
 		var modalChild;
+		var oldModal;
 
 		// If the hash element exists
 		if (modalElement) {
@@ -79,12 +80,18 @@
 			modalChild = modalElement.children[0];
 
 			// When we deal with a modal and body-class `has-overlay` is not set
-			if (modalChild && modalChild.className.match(/modal-inner/) &&
-					!htmlClasses.match(/has-overlay/)) {
+			if (modalChild && modalChild.className.match(/modal-inner/)) {
+				if (!htmlClasses.match(/has-overlay/)) {
 
-				// Set an html class to prevent scrolling
-				document.documentElement.className += ' has-overlay';
+					// Set an html class to prevent scrolling
+					document.documentElement.className += ' has-overlay';
+				}
 
+				// Unmark previous active element
+				if (modal.activeElement) {
+					oldModal = modal.activeElement;
+					oldModal.className = oldModal.className.replace(' is-active', '');
+				}
 				// Mark modal as active
 				modalElement.className += ' is-active';
 				modal.activeElement = modalElement;
@@ -116,8 +123,8 @@
 		}
 	};
 
-	modal._addEventListener(window, 'hashchange', modal.mainHandler, false);
-	modal._addEventListener(window, 'load', modal.mainHandler, false);
+	modal._addEventListener(window, 'hashchange', modal.mainHandler);
+	modal._addEventListener(window, 'load', modal.mainHandler);
 
 	/*
 	 * Accessibility
