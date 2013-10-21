4221647b1b96196d7a8e4f00f177717532997ace
Anselm Hannemann <info@anselm-hannemann.com>
2013-10-22 00:57:50 +0200

    fresher layout, edited content, added stackable modal, etc. (#53)

--- modal.js
+++ modal.js
@@ -3,31 +3,257 @@
  * http://drublic.github.com/css-modal
  *
  * @author Hans Christian Reinl - @drublic
- * @version 1.0.4
+ * @version 1.1.0alpha
  */
 
 (function (global) {
 
 	'use strict';
 
-	// Storage variable
-	var modal = {};
+	/*
+	 * Storage for functions and attributes
+	 */
+	var modal = {
 
-	// Store for currently active element
-	modal.lastActive = undefined;
-	modal.activeElement = undefined;
+		activeElement: undefined, // Store for currently active element
+		lastActive: undefined, // Store for last active elemet
+		stackedElements: [], // Store for stacked elements
 
-	// Polyfill addEventListener for IE8 (only very basic)
-	modal._addEventListener = function (element, event, callback) {
-		if (element.addEventListener) {
-			element.addEventListener(event, callback, false);
-		} else {
-			element.attachEvent('on' + event, callback);
+		// All elements that can get focus, can be tabbed in a modal
+		tabbableElements: 'a[href], area[href], input:not([disabled]),' +
+			'select:not([disabled]), textarea:not([disabled]),' +
+			'button:not([disabled]), iframe, object, embed, *[tabindex],' +
+			'*[contenteditable]',
+
+		/*
+		 * Polyfill addEventListener for IE8 (only very basic)
+		 * @param event {string} event type
+		 * @param element {Node} node to fire event on
+		 * @param callback {function} gets fired if event is triggered
+		 */
+		on: function (event, element, callback) {
+			if (element.addEventListener) {
+				element.addEventListener(event, callback, false);
+			} else {
+				element.attachEvent('on' + event, callback);
+			}
+		},
+
+		/*
+		 * Convenience function to trigger event
+		 * @param event {string} event type
+		 * @param modal {string} id of modal that the event is triggerd on
+		 */
+		trigger: function (event, modal) {
+			var eventTrigger;
+
+			if (!window.CustomEvent) {
+				return;
+			}
+
+			eventTrigger = new CustomEvent(event, {
+				detail: {
+					'modal': modal
+				}
+			});
+
+			document.dispatchEvent(eventTrigger);
+		},
+
+		/*
+		 * Convenience function to add a class to an element
+		 * @param element {Node} element to add class to
+		 * @param className {string}
+		 */
+		addClass: function (element, className) {
+			if (element && !element.className.match(className)) {
+				element.className += ' ' + className;
+			}
+		},
+
+		/*
+		 * Convenience function to remove a class from an element
+		 * @param element {Node} element to remove class off
+		 * @param className {string}
+		 */
+		removeClass: function (element, className) {
+			element.className = element.className.replace(className, '').replace('  ', ' ');
+		},
+
+		/*
+		 * Focus modal
+		 */
+		setFocus: function () {
+			if (modal.activeElement) {
+
+				// Set element with last focus
+				modal.lastActive = document.activeElement;
+
+				// New focussing
+				modal.activeElement.focus();
+
+				// Add handler to keep the focus
+				modal.keepFocus(modal.activeElement);
+			}
+		},
+
+		/*
+		 * Unfocus
+		 */
+		removeFocus: function () {
+			if (modal.lastActive) {
+				modal.lastActive.focus();
+			}
+		},
+
+		/*
+		 * Keep focus inside the modal
+		 * @param element {node} element to keep focus in
+		 */
+		keepFocus: function (element) {
+			var allTabbableElements = element.querySelectorAll(modal.tabbableElements);
+			var firstTabbableElement = allTabbableElements[0];
+			var lastTabbableElement = allTabbableElements[allTabbableElements.length - 1];
+
+			var focusHandler = function (event) {
+				var keyCode = event.which || event.keyCode;
+
+				// TAB pressed
+				if (keyCode !== 9) {
+					return;
+				}
+
+				// Polyfill to prevent the default behavior of events
+				event.preventDefault = event.preventDefault || function () {
+					event.returnValue = false;
+				};
+
+				// Move focus to first element that can be tabbed if Shift isn't used
+				if (event.target === lastTabbableElement && !event.shiftKey) {
+					event.preventDefault();
+					firstTabbableElement.focus();
+
+				// Move focus to last element that can be tabbed if Shift is used
+				} else if (event.target === firstTabbableElement && event.shiftKey) {
+					event.preventDefault();
+					lastTabbableElement.focus();
+				}
+			};
+
+			modal.on('keydown', element, focusHandler);
+		},
+
+		/*
+		 * Mark modal as active
+		 * @param element {Node} element to set active
+		 */
+		setActive: function (element) {
+			modal.addClass(element, 'is-active');
+			modal.activeElement = element;
+
+			// Set the focus to the modal
+			modal.setFocus(element.id);
+
+			// Fire an event
+			modal.trigger('cssmodal:show', modal.activeElement);
+		},
+
+		/*
+		 * Unset previous active modal
+		 * @param isStacked {boolean} true if element is stacked above another
+		 */
+		unsetActive: function (isStacked) {
+			if (modal.activeElement) {
+				modal.removeClass(modal.activeElement, 'is-active');
+
+				// Fire an event
+				modal.trigger('cssmodal:hide', modal.activeElement);
+
+				// Unfocus
+				modal.removeFocus();
+
+				// Make modal stacked if needed
+				if (isStacked) {
+					modal.stackModal(modal.activeElement);
+				}
+
+				// If there are any stacked elements
+				if (!isStacked && modal.stackedElements.length > 0) {
+					modal.unstackModal();
+				}
+
+				// Reset active element
+				modal.activeElement = null;
+			}
+		},
+
+		/*
+		 * Stackable modal
+		 * @param stackableModal {node} element to be stacked
+		 */
+		stackModal: function (stackableModal) {
+			modal.addClass(stackableModal, 'is-stacked');
+
+			// Set modal as stacked
+			modal.stackedElements.push(modal.activeElement);
+		},
+
+		/*
+		 * Reactivate stacked modal
+		 */
+		unstackModal: function () {
+			var stackedCount = modal.stackedElements.length;
+			var lastStacked = modal.stackedElements[stackedCount - 1];
+
+			modal.removeClass(lastStacked, 'is-stacked');
+
+			// Set hash to modal, activates the modal automatically
+			window.location.hash = lastStacked.id;
+
+			// Remove modal from stackedElements array
+			modal.stackedElements.splice(stackedCount - 1, 1);
+		},
+
+		/*
+		 * When displaying modal, prevent background from scrolling
+		 */
+		mainHandler: function () {
+			var hash = window.location.hash.replace('#', '');
+			var modalElement = document.getElementById(hash);
+			var modalChild;
+
+			// If the hash element exists
+			if (modalElement) {
+
+				// Get first element in selected element
+				modalChild = modalElement.children[0];
+
+				// When we deal with a modal and body-class `has-overlay` is not set
+				if (modalChild && modalChild.className.match(/modal-inner/)) {
+
+					// Set an html class to prevent scrolling
+					modal.addClass(document.documentElement, 'has-overlay');
+
+					// Make previous element stackable
+					modal.unsetActive(true);
+
+					// Mark the active element
+					modal.setActive(modalElement);
+				}
+			} else {
+				modal.removeClass(document.documentElement, 'has-overlay');
+
+				// If activeElement is already defined, delete it
+				modal.unsetActive();
+			}
 		}
 	};
 
-	// Hide overlay when ESC is pressed
-	modal._addEventListener(document, 'keyup', function (event) {
+
+	/*
+	 * Hide overlay when ESC is pressed
+	 */
+	modal.on('keyup', document, function (event) {
 		var hash = window.location.hash.replace('#', '');
 
 		// If hash is not set
@@ -48,108 +274,28 @@
 		}
 	}, false);
 
-	// Convenience function to trigger event
-	modal._dispatchEvent = function (event, modal) {
-		var eventTigger;
-
-		if (!document.createEvent) {
-			return;
-		}
-
-		eventTigger = document.createEvent('Event');
-
-		eventTigger.initEvent(event, true, true);
-		eventTigger.customData = { 'modal': modal };
-
-		document.dispatchEvent(eventTigger);
-	};
-
-
-	// When showing overlay, prevent background from scrolling
-	modal.mainHandler = function () {
-		var hash = window.location.hash.replace('#', '');
-		var modalElement = document.getElementById(hash);
-		var htmlClasses = document.documentElement.className;
-		var modalChild;
-		var oldModal;
-
-		// If the hash element exists
-		if (modalElement) {
-
-			// Get first element in selected element
-			modalChild = modalElement.children[0];
-
-			// When we deal with a modal and body-class `has-overlay` is not set
-			if (modalChild && modalChild.className.match(/modal-inner/)) {
-				if (!htmlClasses.match(/has-overlay/)) {
-
-					// Set an html class to prevent scrolling
-					document.documentElement.className += ' has-overlay';
-				}
-
-				// Unmark previous active element
-				if (modal.activeElement) {
-					oldModal = modal.activeElement;
-					oldModal.className = oldModal.className.replace(' is-active', '');
-				}
-				// Mark modal as active
-				modalElement.className += ' is-active';
-				modal.activeElement = modalElement;
-
-				// Set the focus to the modal
-				modal.setFocus(hash);
-
-				// Fire an event
-				modal._dispatchEvent('cssmodal:show', modal.activeElement);
-			}
-		} else {
-			document.documentElement.className =
-					htmlClasses.replace(' has-overlay', '');
-
-			// If activeElement is already defined, delete it
-			if (modal.activeElement) {
-				modal.activeElement.className =
-						modal.activeElement.className.replace(' is-active', '');
-
-				// Fire an event
-				modal._dispatchEvent('cssmodal:hide', modal.activeElement);
-
-				// Reset active element
-				modal.activeElement = null;
-
-				// Unfocus
-				modal.removeFocus();
-			}
-		}
-	};
-
-	modal._addEventListener(window, 'hashchange', modal.mainHandler);
-	modal._addEventListener(window, 'load', modal.mainHandler);
 
 	/*
-	 * Accessibility
+	 * Trigger main handler on load and hashchange
+	 */
+	modal.on('hashchange', window, modal.mainHandler);
+	modal.on('load', window, modal.mainHandler);
+
+	/*
+	 * AMD, module loader, global registration
 	 */
 
-	// Focus modal
-	modal.setFocus = function () {
-		if (modal.activeElement) {
+	// Expose modal for loaders that implement the Node module pattern.
+	if (typeof module === 'object' && module && typeof module.exports === 'object') {
+		module.exports = modal;
 
-			// Set element with last focus
-			modal.lastActive = document.activeElement;
-
-			// New focussing
-			modal.activeElement.focus();
-		}
-	};
-
-	// Unfocus
-	modal.removeFocus = function () {
-		if (modal.lastActive) {
-			modal.lastActive.focus();
-		}
-	};
+	// Register as an AMD module
+	} else if (typeof define === 'function' && define.amd) {
+		define([], function () { return modal; });
 
 	// Export CSSModal into global space
-	global.CSSModal = modal;
+	} else if (typeof global === 'object' && typeof global.document === 'object') {
+		global.CSSModal = modal;
+	}
 
 }(window));
