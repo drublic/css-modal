25e898079eb2090032a08dec89da0c6b62cb3276
Anselm Hannemann <info@anselm-hannemann.com>
2014-04-15 18:20:58 +0200

    Fix resizing function to work with AMD, etc.

--- plugins/resizemodal.js
+++ plugins/resizemodal.js
@@ -1,39 +1,19 @@
 /*
  * CSS Modal Plugin for dynamic modal resizing
  * Author: Anselm Hannemann
- * Date: 2014-02-07
- *
- * @TODO:
- * - Fix text modal and video modal centering
- * - Set styles properly
- * - Improve resizing animation
- * - matchMedia Fallback
+ * Date: 2014-02-04
  */
 
-(function (global) {
+var resizeModalDynamically = function (CSSModal) {
 
 	'use strict';
-
-	var CSSModal = global.CSSModal;
 
 	// If CSS Modal is still undefined, throw an error
 	if (!CSSModal) {
 		throw new Error('Error: CSSModal is not loaded.');
 	}
 
-	var matchMedia = function (media) {
-		if (window.matchMedia && window.matchMedia('all').addListener) {
-			console.log('matchMedia is supported');
-			return false;
-		}
-		else {
-			// window.innerWidth(event, callback);
-			console.log('matchMedia is not supported');
-			// return mediaQueryList;
-		}
-	};
-
-	var throttle = function (func, wait, immediate) {
+	var throttle = function(func, wait, immediate) {
 		var timeout;
 
 		return function() {
@@ -55,65 +35,55 @@
 		};
 	};
 
-	var fixCloseBtn = function () {
-		var $modalClose = CSSModal.activeElement.querySelector('.modal-close');
-		var $modalContent = CSSModal.activeElement.querySelector('.modal-content');
-
-		var newClose = $modalClose.cloneNode();
-
-		// @TODO: Set missing styles for .modal-close--inner
-		var newCloseStyles = 'position: absolute; top: 0; right: 0; padding: 15px; font-size: 2em; line-height: 0.5; border-radius: 2px; background: #fff; color: #111; text-decoration: none;';
-
-		newClose.className = 'modal-close--inner';
-		newClose.setAttribute('style', newCloseStyles);
-		newClose.innerHTML = '&#10799;';
-		$modalContent.appendChild(newClose);
-		$modalClose.parentNode.removeChild($modalClose);
-	};
-
-	var resizeModal = function (e) {
-		// Only apply dynamic resize if modal is not in mobile view
-		if (window.matchMedia('(max-width: 30em)').matches) {
-			return;
-		}
-
+	var resizeModal = function(e) {
 		var windowHeight = window.innerHeight;
 		var windowWidth = window.innerWidth;
 		var margin = 40;
-		var $modalInner = CSSModal.activeElement.querySelector('.modal-inner');
-		var $modalContent = $modalInner.querySelector('.modal-content');
-		var innerHeight = $modalInner.scrollHeight;
-		var innerWidth = $modalInner.scrollWidth;
 
-		var contentHeight = $modalContent.scrollHeight;
-		var contentWidth = $modalContent.scrollWidth;
-		var ratio = (contentWidth / contentHeight);
-
-		// If element is higher than viewport
-		if (contentHeight > (innerHeight - (2 * margin))) {
-			// Set height of .modal-inner and .modal-content
-			var padding = 28; // padding in CSS is 15px
-
-			var newHeight = (windowHeight - (2 * margin));
-			var newWidth = parseInt((newHeight * ratio), 10);
-
-			$modalInner.style.maxWidth = newWidth + 'px';
-			$modalInner.style.width = 'auto';
-			$modalInner.style.maxHeight = newHeight + 'px';
-			$modalInner.style.marginLeft = ((newWidth) / 2)*-1 + 'px';
-			$modalContent.style.maxHeight = newHeight + 'px';
+		// If width smaller than height (Portrait mode)
+		if (windowWidth < windowHeight) {
+			// Resize modal and images/video/iframe accordingly respecting aspect-ratio
+			CSSModal.activeElement.querySelector('.modal-inner').style.width = (windowWidth - (2 * margin)) + 'px';
+			CSSModal.activeElement.querySelector('.modal-inner').style.marginLeft = ((windowWidth - (2 * margin)) / 2)*-1 + 'px';
+		}
+		// If width wider than height (Landscape mode)
+		else if (windowWidth > windowHeight) {
+			// Resize modal and images/video/iframe accordingly respecting aspect-ratio
+			CSSModal.activeElement.getElementsByClassName('modal-inner')[0].style.maxHeight = (windowHeight - (2 * margin)) + 'px';
 		}
 	};
 
-	// Function to call the resizing throttled
-	var resizeModalThrottled = throttle(resizeModal, 5);
+	var resizeModalThrottled = throttle(resizeModal, 500);
 
-	CSSModal.on('cssmodal:show', document, function (event) {
-		fixCloseBtn();
+	// @TODO: MatchMedia to not affect mobile view (<690px vw)
+	// @TODO: .modal-close:after pseudo-element position margin-left (http://stackoverflow.com/questions/7330355/javascript-set-css-after-styles/7330454#7330454)
+
+	CSSModal.on('readystatechange', document, function (event) {
+		resizeModal();
+	});
+
+	CSSModal.on('resize', document, function (event) {
 		resizeModalThrottled();
 	});
 
-	CSSModal.on('resize', window, function (event) {
-		resizeModalThrottled();
-	});
-}(window));
+};
+
+/*
+ * AMD, module loader, global registration
+ */
+
+// Expose modal for loaders that implement the Node module pattern.
+if (typeof module === 'object' && module && typeof module.exports === 'object') {
+    module.exports = {};
+
+// Register as an AMD module
+} else if (typeof define === 'function' && define.amd) {
+
+define(['CSSModal'], function (CSSModal) {
+    resizeModalDynamically(CSSModal);
+});
+
+// Export CSSModal into global space
+} else if (typeof global === 'object' && typeof global.document === 'object') {
+    resizeModalDynamically(CSSModal);
+}
