0611f50f78513ca7aff912e4a29e4f931556a4bb
Anselm Hannemann <info@anselm-hannemann.com>
2014-04-15 18:41:51 +0200

    More refactoring of the resize plugin code

--- plugins/resizemodal.js
+++ plugins/resizemodal.js
@@ -1,19 +1,19 @@
 /*
  * CSS Modal Plugin for dynamic modal resizing
  * Author: Anselm Hannemann
- * Date: 2014-02-04
+ * Date: 2014-04-15
  */
 
-var resizeModalDynamically = function (CSSModal) {
+'use strict';
 
-	'use strict';
+var resizeModalDynamically = function (CSSModal) {
 
 	// If CSS Modal is still undefined, throw an error
 	if (!CSSModal) {
 		throw new Error('Error: CSSModal is not loaded.');
 	}
 
-	var throttle = function(func, wait, immediate) {
+	var throttle = function (func, wait, immediate) {
 		var timeout;
 
 		return function() {
@@ -35,19 +35,16 @@ var resizeModalDynamically = function (CSSModal) {
 		};
 	};
 
-	var resizeModal = function(e) {
+	var resizeModal = function () {
 		var windowHeight = window.innerHeight;
 		var windowWidth = window.innerWidth;
 		var margin = 40;
 
-		// If width smaller than height (Portrait mode)
-		if (windowWidth < windowHeight) {
+		if (windowWidth < windowHeight) { // If width smaller than height
 			// Resize modal and images/video/iframe accordingly respecting aspect-ratio
 			CSSModal.activeElement.querySelector('.modal-inner').style.width = (windowWidth - (2 * margin)) + 'px';
 			CSSModal.activeElement.querySelector('.modal-inner').style.marginLeft = ((windowWidth - (2 * margin)) / 2)*-1 + 'px';
-		}
-		// If width wider than height (Landscape mode)
-		else if (windowWidth > windowHeight) {
+		} else if (windowWidth > windowHeight) { // If width wider than height
 			// Resize modal and images/video/iframe accordingly respecting aspect-ratio
 			CSSModal.activeElement.getElementsByClassName('modal-inner')[0].style.maxHeight = (windowHeight - (2 * margin)) + 'px';
 		}
@@ -57,16 +54,15 @@ var resizeModalDynamically = function (CSSModal) {
 
 	// @TODO: MatchMedia to not affect mobile view (<690px vw)
 	// @TODO: .modal-close:after pseudo-element position margin-left (http://stackoverflow.com/questions/7330355/javascript-set-css-after-styles/7330454#7330454)
-
-	CSSModal.on('readystatechange', document, function (event) {
-		resizeModal();
-	});
-
-	CSSModal.on('resize', document, function (event) {
-		resizeModalThrottled();
-	});
-
 };
+
+CSSModal.on('readystatechange', document, function (event) {
+	resizeModalDynamically();
+});
+
+CSSModal.on('resize', document, function (event) {
+	resizeModalDynamically();
+});
 
 /*
  * AMD, module loader, global registration
@@ -74,16 +70,16 @@ var resizeModalDynamically = function (CSSModal) {
 
 // Expose modal for loaders that implement the Node module pattern.
 if (typeof module === 'object' && module && typeof module.exports === 'object') {
-    module.exports = {};
+	module.exports = {};
 
 // Register as an AMD module
 } else if (typeof define === 'function' && define.amd) {
 
 define(['CSSModal'], function (CSSModal) {
-    resizeModalDynamically(CSSModal);
+	resizeModalDynamically(CSSModal);
 });
 
 // Export CSSModal into global space
 } else if (typeof global === 'object' && typeof global.document === 'object') {
-    resizeModalDynamically(CSSModal);
+	resizeModalDynamically(CSSModal);
 }
