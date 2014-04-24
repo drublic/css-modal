a75ce12bd186e13ce9ee335a3f6f17862fe3d51e
Hans Christian Reinl <info@drublic.de>
2014-04-24 09:48:02 +0200

    Fix HTML5 Video plugin JSHint errors

--- plugins/html5video.js
+++ plugins/html5video.js
@@ -9,31 +9,31 @@
 	'use strict';
 
 	var CSSModal = global.CSSModal;
+	var videos;
 
 	// If CSS Modal is still undefined, throw an error
 	if (!CSSModal) {
 		throw new Error('Error: CSSModal is not loaded.');
 	}
 
-	var videos;
-
 	// Enables Auto-Play when calling modal
-	CSSModal.on('cssmodal:show', document, function (event) {
+	CSSModal.on('cssmodal:show', document, function () {
+
 		// Fetch all video elements in active modal
 		videos = CSSModal.activeElement.querySelectorAll('video');
 
+		// Play first video in modal
 		if (videos.length > 0) {
-			// Play first video in modal
 			videos[0].play();
 		}
 	});
 
 	// If modal is closed, pause all videos
-	CSSModal.on('cssmodal:hide', document, function (event) {
+	CSSModal.on('cssmodal:hide', document, function () {
 		var i = 0;
 
+		// Pause all videos in active modal
 		if (videos.length > 0) {
-			// Pause all videos in active modal
 			for (; i < videos.length; i++) {
 				videos[i].pause();
 			}
