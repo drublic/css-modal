1ce85d4c993acfe01ded5008ec729eb2391e0e6f
Jonathan Weiß <bitbucket@jonathanweiss.net>
2014-05-19 16:46:12 +0200

    Fix async tests

--- test/spec/gallery-spec.js
+++ test/spec/gallery-spec.js
@@ -1,4 +1,4 @@
-/*global describe, it, expect, Event*/
+/*global describe, it, expect, Event, waits, runs*/
 (function ($) {
 
 	'use strict';
@@ -7,48 +7,68 @@
 	describe('Gallery plugin', function () {
 
 		it('can open a modal', function () {
-			var element = $('ul > li').eq(1).find('a').get(0);
-			var event = new Event('click', {'cancelable': true});
-			element.dispatchEvent(event);
+			runs(function () {
+				var element = $('ul > li').eq(1).find('a').get(0);
+				var event = new Event('click', {'cancelable': true});
+				element.dispatchEvent(event);
 
-			setTimeout(function () {
-				expect($('#modal-gallery').css('opacity')).toBe('1');
-			}, 10);
+				setTimeout(function () {
+					expect($('#modal-gallery-boring').css('opacity')).toBe('1');
+				}, 500);
+			});
+
+			waits(750);
+
 		});
 
 		it('can navigate forwards by clicking', function () {
-			var element = $('#modal-gallery-boring a.modal-next').get(0);
-			var event = new Event('click', {'cancelable': true});
-			element.dispatchEvent(event);
+			runs(function () {
+				var element = $('#modal-gallery-boring a.modal-next').get(0);
+				var event = new Event('click', {'cancelable': true});
+				element.dispatchEvent(event);
 
-			event = new Event('click', {'cancelable': true});
-			element.dispatchEvent(event);
+				event = new Event('click', {'cancelable': true});
+				element.dispatchEvent(event);
 
-			window.setTimeout(function () {
-				var src = $('#modal-gallery-boring .big-image img').attr('src');
-				expect(src).toMatch('http://dummyimage.com/400x300&text=3+-+6-dolor+sit+amet');
-			}, 0);
+				window.setTimeout(function () {
+					var src = $('#modal-gallery-boring .big-image img').attr('src');
+					expect(src).toEqual('http://dummyimage.com/400x300&text=3+-+6-consectetur');
+				}, 0);
+			});
+
+			waits(20);
 		});
 
 		it('can navigate backwards by clicking', function () {
-			var element = $('#modal-gallery-boring a.modal-prev').get(0);
-			var event = new Event('click', {'cancelable': true});
-			element.dispatchEvent(event);
+			runs(function () {
+				var element = $('#modal-gallery-boring a.modal-prev').get(0);
+				var event = new Event('click', {'cancelable': true});
+				element.dispatchEvent(event);
 
-			window.setTimeout(function () {
-				var src = $('#modal-gallery-boring .big-image img').attr('src');
-				expect(src).toMatch('http://dummyimage.com/400x300&text=2+-+6-dolor+sit+amet');
-			}, 0);
+				window.setTimeout(function () {
+					var src = $('#modal-gallery-boring .big-image img').attr('src');
+					expect(src).toEqual('http://dummyimage.com/400x300&text=2+-+6-dolor+sit+amet');
+				}, 0);
+			});
+
+			waits(10);
 		});
 
 		it('can close the gallery', function () {
-			var element = $('#modal-gallery-boring .modal-close').get(0);
-			var event = new Event('click', {'cancelable': true});
-			element.dispatchEvent(event);
+			runs(function () {
+				var element = $('#modal-gallery-boring .modal-close').get(0);
+				var event = new Event('click', {'cancelable': true});
+				element.dispatchEvent(event);
+				setTimeout(function () {
+					expect($('#modal-gallery-boring').css('opacity')).toBe('0');
+				}, 750);
+			});
+
+			waits(800);
 		});
 
 		it('can open a diffent gallery', function () {
-			window.setTimeout(function () {
+			runs(function () {
 				var element = $('ul > li').eq(0).find('a').get(0);
 				var event = new Event('click', {'cancelable': true});
 				element.dispatchEvent(event);
@@ -57,35 +77,41 @@
 					expect($('#modal-gallery').css('opacity')).toBe('1');
 					var src = $('#modal-gallery .big-image img').attr('src');
 					expect(src).toMatch('http://placekitten.com/400/300');
-				}, 10);
-			},0);
+				}, 500);
+			});
+
+			waits(750);
 		});
 
 		it('restores the first gallery correctly, after it had been closed', function () {
-			window.setTimeout(function () {
-				var element = $('#modal-gallery a.modal-next').get(0);
-				var event = new Event('click', {'cancelable': true});
-				element.dispatchEvent(event);
+			runs(function () {
+				window.setTimeout(function () {
+					var element = $('#modal-gallery a.modal-next').get(0);
+					var event = new Event('click', {'cancelable': true});
+					element.dispatchEvent(event);
 
-				element = $('#modal-gallery .modal-close').get(0);
-				event = new Event('click', {'cancelable': true});
-				element.dispatchEvent(event);
-			}, 20);
+					element = $('#modal-gallery .modal-close').get(0);
+					event = new Event('click', {'cancelable': true});
+					element.dispatchEvent(event);
+				}, 20);
 
-			window.setTimeout(function () {
-				var element = $('ul > li').eq(1).find('a').get(0);
-				var event = new Event('click', {'cancelable': true});
-				element.dispatchEvent(event);
+				window.setTimeout(function () {
+					var element = $('ul > li').eq(1).find('a').get(0);
+					var event = new Event('click', {'cancelable': true});
+					element.dispatchEvent(event);
 
-				var src = $('#modal-gallery-boring .big-image img').attr('src');
-				expect(src).toMatch('http://dummyimage.com/400x300&text=2+-+6-dolor+sit+amet');
-			}, 40);
+					var src = $('#modal-gallery-boring .big-image img').attr('src');
+					expect(src).toEqual('http://dummyimage.com/400x300&text=2+-+6-dolor+sit+amet');
+				}, 40);
+			});
+
+			waits(100);
 		});
 
 		// Hide the last open gallery
 		window.setTimeout(function(){
 			window.location.hash = '';
-		}, 500);
+		}, 4000);
 
 	});
 
