6a1cb79572b6f9c6ed76338bd8de68d87629d525
Hans Christian Reinl <info@drublic.de>
2014-05-21 11:46:29 +0200

    Update plugin HTML5 Video to work with AMD definition

--- plugins/html5video.js
+++ plugins/html5video.js
@@ -8,35 +8,66 @@
 
 	'use strict';
 
-	var CSSModal = global.CSSModal;
-	var videos;
+	/**
+	 * Main modal object
+	 */
+	var CSSModal;
 
-	// If CSS Modal is still undefined, throw an error
-	if (!CSSModal) {
-		throw new Error('Error: CSSModal is not loaded.');
-	}
+	/**
+	 * Initial call
+	 */
+	var init = function (modal) {
+		CSSModal = modal;
 
-	// Enables Auto-Play when calling modal
-	CSSModal.on('cssmodal:show', document, function () {
-
-		// Fetch all video elements in active modal
-		videos = CSSModal.activeElement.querySelectorAll('video');
-
-		// Play first video in modal
-		if (videos.length > 0) {
-			videos[0].play();
+		// If CSS Modal is still undefined, throw an error
+		if (!CSSModal) {
+			throw new Error('Error: CSSModal is not loaded.');
 		}
-	});
 
-	// If modal is closed, pause all videos
-	CSSModal.on('cssmodal:hide', document, function () {
-		var i = 0;
+		var videos;
 
-		// Pause all videos in active modal
-		if (videos.length > 0) {
-			for (; i < videos.length; i++) {
-				videos[i].pause();
+		// Enables Auto-Play when calling modal
+		CSSModal.on('cssmodal:show', document, function () {
+
+			// Fetch all video elements in active modal
+			videos = CSSModal.activeElement.querySelectorAll('video');
+
+			// Play first video in modal
+			if (videos.length > 0) {
+				videos[0].play();
 			}
-		}
-	});
+		});
+
+		// If modal is closed, pause all videos
+		CSSModal.on('cssmodal:hide', document, function () {
+			var i = 0;
+
+			// Pause all videos in active modal
+			if (videos.length > 0) {
+				for (; i < videos.length; i++) {
+					videos[i].pause();
+				}
+			}
+		});
+
+		return {};
+	};
+
+	/*
+	 * AMD, module loader, global registration
+	 */
+
+	// Expose modal for loaders that implement the Node module pattern.
+	if (typeof module === 'object' && module && typeof module.exports === 'object') {
+		module.exports = {};
+
+	// Register as an AMD module
+	} else if (typeof define === 'function' && define.amd) {
+
+		define(['CSSModal'], init);
+
+	// Export CSSModal into global space
+	} else if (typeof global === 'object' && typeof global.document === 'object') {
+		init(global.CSSModal);
+	}
 }(window));
