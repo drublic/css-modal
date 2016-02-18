f4ac55f961ea7b35af0f5a34647050b11140ea2a
Hugo Giraudel <hugo.giraudel@gmail.com>
2016-02-18 13:03:19 +0100

    Improved tabbableElements selector string

--- modal.js
+++ modal.js
@@ -21,8 +21,8 @@
 		// All elements that can get focus, can be tabbed in a modal
 		tabbableElements: 'a[href], area[href], input:not([disabled]),' +
 			'select:not([disabled]), textarea:not([disabled]),' +
-			'button:not([disabled]), iframe, object, embed, *[tabindex],' +
-			'*[contenteditable]',
+			'button:not([disabled]), iframe, object, embed, [tabindex]:not([tabindex="-1"]),' +
+			'[contenteditable]',
 
 		/*
 		 * Polyfill addEventListener for IE8 (only very basic)
