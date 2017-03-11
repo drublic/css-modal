7c97d0d472f27c2de69d95cfefdd228bc0c11d31
Hans Christian Reinl <info@drublic.de>
2017-03-11 19:24:22 +0100

    First draft to v2

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: './bin/',
+    filename: 'index.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+};
