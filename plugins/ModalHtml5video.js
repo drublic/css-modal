50befee995cad121aca5549b250d84775033ede1
Hans Christian Reinl <info@drublic.de>
2017-04-14 09:23:29 +0200

    Rewrite plugin html5 Video

---
+++ plugins/ModalHtml5video.js
@@ -0,0 +1,50 @@
+/*
+ * CSS Modal Plugin for HTML5 Video (play/pause)
+ */
+import Modal from '../js/Modal.js';
+
+class ModalHtml5video extends Modal {
+  constructor () {
+    super();
+
+    this.videos;
+
+    // Enables Auto-Play when calling modal
+    this.Helpers.on('cssmodal:show', document, this._show);
+
+    // If modal is closed, pause all videos
+    this.Helpers.on('cssmodal:hide', document, this._hide);
+  }
+
+  /**
+   * Show modal
+   */
+  _show () {
+
+    // Fetch all video elements in active modal
+    try {
+      this.videos = this.activeElement.querySelectorAll('video');
+    } catch (ex) {
+      return;
+    }
+
+    // Play first video in modal
+    if (this.videos.length > 0) {
+      this.videos[0].play();
+    }
+  }
+
+  /**
+   * Hide modal
+   */
+  _hide () {
+    // Pause all videos in active modal
+    if (this.videos.length && this.videos.length > 0) {
+      for (let i = 0; i < this.videos.length; i++) {
+        this.videos[i].pause();
+      }
+    }
+  }
+}
+
+export default ModalHtml5video;
